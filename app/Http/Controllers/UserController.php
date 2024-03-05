276ed4341c4f800c7230e9a5471b0bacb9f0fdb4
raihanachmad8 <raihanachmad8@gmail.com>
2024-03-05 09:33:18 +0700

    jobsheet(4): Praktikum 1 -

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,18 +9,13 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        // ];
-        // UserModel::insert($data);
-
-        // $data = [
-        //     'nama' => 'Pelanggan Pertama',
-        // ];
-        // UserModel::where('username', 'customer-1')->update($data);
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
+        ];
+        UserModel::create($data);
 
         $user = UserModel::all();
         return view('user', ['data' => $user]);
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,4 +11,6 @@ class UserModel extends Model
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
+
+    protected $fillable = ['level_id', 'username', 'nama'];
 }
