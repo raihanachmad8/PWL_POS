f91496cfb28c28d6001ba5702e782fb7e9c20bb2
raihanachmad8 <raihanachmad8@gmail.com>
2024-03-05 10:46:23 +0700

    jobsheet(4): Praktikum 2.3  Retreiving Aggregrates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
     public function index() {
 
 
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        $user = UserModel::where('level_id', '2')->count();
         return view('user', ['data' => $user]);
     }
 }
