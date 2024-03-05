264bcfd96e72e4eaff098bb4d80dd4cd967d9c83
raihanachmad8 <raihanachmad8@gmail.com>
2024-03-05 10:25:59 +0700

    jobsheet(4): Praktikum 2.1  Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,15 +9,11 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::create($data);
 
-        $user = UserModel::all();
+
+        $user = UserModel::findOr(20, ['username', 'nama'], function() {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
