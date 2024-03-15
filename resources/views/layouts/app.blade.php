5f013557e457b1db149dadbedb524e6dafa6351b
raihanachmad8 <raihanachmad8@gmail.com>
2024-03-15 15:56:38 +0700

    jobsheet(5): Praktikum 2  Integrasi dengan DataTables

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,11 +2,13 @@
 {{-- Extend and customize the browser title --}}
 @section('title')
     {{ config('adminlte.title') }}
-    @hasSection('subtitle')
-        | @yield('subtitle')
-    @endif
+    @hasSection('subtitle') | @yield('subtitle') @endif
 @stop
+
+@vite('resources/js/app.js')
+
 {{-- Extend and customize the page content header --}}
+
 @section('content_header')
     @hasSection('content_header_title')
         <h1 class="text-muted">
@@ -37,14 +39,14 @@
 @stop
 {{-- Add common Javascript/Jquery code --}}
 @push('js')
-    <script>
-        $(document).ready(function() {
-            // Add your common script logic here...
-        });
-    </script>
+<script src="https://cdn.datatables.net/2.0.2/js/dataTables.js"></script>
 @endpush
+
+@stack('scripts')
+
 {{-- Add common CSS customizations --}}
 @push('css')
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.dataTables.css">
     <style type="text/css">
         {{-- You can add AdminLTE customizations here --}}
         /*
